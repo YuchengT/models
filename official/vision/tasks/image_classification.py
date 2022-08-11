b22ea8fdc9cf3f1bbd22fd6b018d25324b4cefe4
Yucheng Tu <109311506+YuchengT@users.noreply.github.com>
2022-08-11 15:17:37 -0700

    Update image_classification.py

--- official/vision/tasks/image_classification.py
+++ official/vision/tasks/image_classification.py
@@ -243,17 +243,17 @@ class ImageClassificationTask(base_task.Task):
 
       # For mixed_precision policy, when LossScaleOptimizer is used, loss is
       # scaled for numerical stability.
-      if isinstance(
-          optimizer, tf.keras.mixed_precision.LossScaleOptimizer):
-        scaled_loss = optimizer.get_scaled_loss(scaled_loss)
+      #if isinstance(
+      #   optimizer, tf.keras.mixed_precision.LossScaleOptimizer):
+      # scaled_loss = optimizer.get_scaled_loss(scaled_loss)
 
     tvars = model.trainable_variables
     grads = tape.gradient(scaled_loss, tvars)
     # Scales back gradient before apply_gradients when LossScaleOptimizer is
     # used.
-    if isinstance(
-        optimizer, tf.keras.mixed_precision.LossScaleOptimizer):
-      grads = optimizer.get_unscaled_gradients(grads)
+    #if isinstance(
+    #   optimizer, tf.keras.mixed_precision.LossScaleOptimizer):
+    # grads = optimizer.get_unscaled_gradients(grads)
     
     return outputs, scaled_loss, tvars, grads
     
